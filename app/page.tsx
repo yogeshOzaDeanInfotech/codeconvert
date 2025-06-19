34173bfb37e171a1da794e9bfc6cfb704dec5b11
yogeshOzaDeanInfotech <o.yogesh@deaninfotech.com>
2025-06-19 23:12:58 +0530

    Add dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen">
+    <main className="flex items-center justify-center gap-4 min-h-screen">
       <Link href="/login" className="text-blue-600 underline">
         Go to Login
+      </Link>
+      <Link href="/dashboard" className="text-blue-600 underline">
+        Go to Dashboard
       </Link>
     </main>
   );
